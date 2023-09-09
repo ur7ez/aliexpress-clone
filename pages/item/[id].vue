ff4c8b97a2d1b18ac87fd38f38178940116bbea0
ur7ez <ur7ez@ukr.net>
2023-09-09 22:53:50 +0300

    complete product item page

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -31,7 +31,7 @@
 
             <div class="flex items-center pt-1.5">
               <span class="h-4 min-w-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
-                <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12" />
+                <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12"/>
               </span>
               <p class="text-[#FF5353]">Extra 5% off</p>
             </div>
@@ -51,6 +51,23 @@
               <div class="text-xl font-bold">$ {{ priceComputed }}</div>
               <span class="bg-[#F5F5F5] border text-[#C08562] text-[9px] font-semibold px-1.5 rounded-sm">70% off</span>
             </div>
+
+            <p class="text-[#009A66] text-xs font-semibold pt-1">
+              Free 11-day delivery over $8.28
+            </p>
+            <p class="text-[#009A66] text-xs font-semibold pt-1">
+              Free Shipping
+            </p>
+
+            <div class="py-2"/>
+            <button
+                @click="addToCart()"
+                :disabled="isInCart"
+                class="px-6 py-2 rounded-lg text-white text-lg font-semibold bg-gradient-to-r from-[#FF851A] to-[#FFAC2C]"
+            >
+              <div v-if="isInCart">Is Added</div>
+              <div v-else>Add to Cart</div>
+            </button>
           </div>
         </div>
       </div>
@@ -60,8 +77,13 @@
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
+import {useUserStore} from "~/stores/user";
+import {useRoute} from "nuxt/app";
 
-let currentImage = ref(null)
+const userStore = useUserStore();
+const route = useRoute();
+
+let currentImage = ref(null);
 
 onMounted(() => {
   watchEffect(() => {
@@ -70,6 +92,15 @@ onMounted(() => {
   })
 })
 
+const isInCart = computed(() => {
+  let res = false;
+  userStore.cart.forEach(prod => {
+    if (route.params.id === prod.id) {
+      res = true;
+    }
+  })
+  return res;
+})
 const priceComputed = computed(() => {
   return '26.40'
 })
@@ -82,4 +113,8 @@ const images = ref([
   'https://picsum.photos/id/99/800/800',
   'https://picsum.photos/id/144/800/800',
 ])
+
+const addToCart = () => {
+  alert('ADDED');
+}
 </script>
